7ddbe8d0707b26ef6bc5b737f091ba5ff45b4806
LuciusYu <LuciusYuNju@gmail.com>
2014-10-21 17:01:41 +0800

    store pairs in file

--- src/test/java/com/intel/picklepot/perf/InputUtils.java
+++ src/test/java/com/intel/picklepot/perf/InputUtils.java
@@ -2,8 +2,7 @@ package com.intel.picklepot.perf;
 
 import com.intel.picklepot.Pair;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 import java.util.Map.Entry;
 
@@ -12,35 +11,59 @@ public class InputUtils {
   private static List<Pair> pairs;
   private static long dataSize;
 
-  static List<Pair> getPairs() throws FileNotFoundException {
+  static List<Pair> getPairs() throws IOException {
     if(pairs != null)
       return pairs;
-
-    Scanner in = new Scanner(inFile);
-    Map<String, Integer> map = new HashMap<String, Integer>();
-    while (in.hasNext()) {
-      for (String str : in.nextLine().split("[^a-zA-Z]")) {
-        str = str.toLowerCase();
-        if (!map.containsKey(str)) {
-          map.put(str, 1);
-        } else {
-          map.put(str, map.get(str) + 1);
-        }
-      }
-    }
-    in.close();
-
-    pairs = new LinkedList<Pair>();
-    for (Entry<String, Integer> entry : map.entrySet()) {
-      Pair p = new Pair(entry.getKey(), entry.getValue());
+    pairs = new ArrayList<Pair>();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("pairs.data"))));
+    String line;
+    while((line = reader.readLine()) != null){
+      String[] strs = line.split(" ");
+      Pair p = new Pair(strs[0], Integer.parseInt(strs[1]));
       pairs.add(p);
-      dataSize += p.word.length() + 4;
     }
-    System.out.printf("dataSize:%,d\n", dataSize);
     return pairs;
   }
 
   public static long getDataSize() {
     return dataSize;
   }
+
+//  static List<Pair> getPairs() throws FileNotFoundException {
+//    if(pairs != null)
+//      return pairs;
+//    Scanner in = new Scanner(inFile);
+//    Map<String, Integer> map = new HashMap<String, Integer>();
+//    while (in.hasNext()) {
+//      for (String str : in.nextLine().split("[^a-zA-Z]")) {
+//        str = str.toLowerCase();
+//        if(str.equals(""))
+//          continue;
+//        if (!map.containsKey(str)) {
+//          map.put(str, 1);
+//        } else {
+//          map.put(str, map.get(str) + 1);
+//        }
+//      }
+//    }
+//    in.close();
+//
+//    pairs = new LinkedList<Pair>();
+//    for (Entry<String, Integer> entry : map.entrySet()) {
+//      Pair p = new Pair(entry.getKey(), entry.getValue());
+//      pairs.add(p);
+//      dataSize += p.word.length() + 4;
+//    }
+//    System.out.printf("dataSize:%,d\n", dataSize);
+//    return pairs;
+//  }
+
+//  public static void main(String args[]) throws IOException {
+//    List<Pair> pairs = getPairs();
+//    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("pairs.data"))));
+//    for(Pair p : pairs)
+//      writer.write(p.word + " " + p.count + "\n");
+//    writer.flush();
+//    writer.close();
+//  }
 }
