79cf6eaedf37bb08c6d8bf723c877546f9761719
LuciusYu <LuciusYuNju@gmail.com>
2014-10-28 09:44:47 +0800

    support serializing byte[] or classes that contains non-string & non-integer fields.

---
+++ src/main/java/com/intel/picklepot/columnar/JavaEncoder.java
@@ -0,0 +1,45 @@
+package com.intel.picklepot.columnar;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Iterator;
+
+/**
+ * This is an alternative to encode non-integer and non-string fields.
+ * @param <T>
+ */
+public class JavaEncoder<T> implements Encoder<T>{
+  private ByteArrayOutputStream baos = new ByteArrayOutputStream();
+  private ObjectOutputStream oos;
+
+  public JavaEncoder() {
+    try {
+      oos = new ObjectOutputStream(baos);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public OutputStream getOutputStream() {
+    return baos;
+  }
+
+  @Override
+  public void encode(Iterator<T> values) {
+    while(values.hasNext()) {
+      try {
+        oos.writeObject(values.next());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
+  public void encode(Object value) {
+
+  }
+}
