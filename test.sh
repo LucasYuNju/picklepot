14a579342857c06e3180692082fbdfe42911bc66
LuciusYu <LucasYuNju@gmail.com>
2014-12-01 14:04:48 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 #prepare data
-echo "\nPreparing data"
-rm -r tpch-dbgen
-git clone https://github.com/electrum/tpch-dbgen.git
-cd tpch-dbgen
-make -s 2>>logs
-./dbgen -s 1
-cd ..
-mkdir data
-cp tpch-dbgen/orders.tbl data/
+if [ ! -f "data/orders.tbl" ]
+then
+    echo "\nPreparing data"
+    rm -rf tpch-dbgen
+    git clone https://github.com/electrum/tpch-dbgen.git
+    cd tpch-dbgen
+    make -s 2>>logs
+    ./dbgen -s 1
+    cd ..
+    mkdir data
+    cp tpch-dbgen/orders.tbl data/
+fi
 
 #build and test
-export MAVEN_OPTS="-Xms1024m -Xmx4096m"
 BUILD_COMMAND="mvn -Dtest=TestPerf test"
 echo "\nTesting with..."
 echo "\$ $BUILD_COMMAND\n"
 
 ${BUILD_COMMAND}
-
