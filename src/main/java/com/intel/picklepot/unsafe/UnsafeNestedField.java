734a953ec880b64ab505368ad03662e95aa37f8d
Yu Yang <LucasYuNju@gmail.com>
2015-01-22 14:11:16 +0800

    Update UnsafeNestedField.java

--- src/main/java/com/intel/picklepot/unsafe/UnsafeNestedField.java
+++ src/main/java/com/intel/picklepot/unsafe/UnsafeNestedField.java
@@ -7,41 +7,36 @@ import com.intel.picklepot.exception.PicklePotException;
  * UnsafeObjectField can serialize object in this field with Java built-in serializer,
  * or unfold object and call FieldGroup to handle it.
  */
-public class UnsafeNestedField extends UnsafeField{
+public class UnsafeNestedField extends UnsafeField {
   private FieldGroup group;
 
   public UnsafeNestedField(Object object, long offset, NewPicklePotImpl picklepot) {
     super(object.getClass(), offset, picklepot);
-    if(Utils.toFieldType(clazz) == FieldType.NESTED) {
-      group = new FieldGroup(object, picklePot);
-    }
+    group = new FieldGroup(object, picklePot);
   }
 
   @Override
   public void write(Object object) throws PicklePotException {
-    if(group != null) {
-      group.write(object);
-    }
-    else {
-      if(writer == null) {
-        writer = Utils.getColumnWriter(clazz, picklePot.getOutput());
-      }
-      Object writeVal = Utils.getUnsafe().getObject(object, offset);
-      writer.write(writeVal);
-    }
+    Object fieldObj = Utils.getUnsafe().getObject(object, offset);
+    group.write(fieldObj);
   }
 
   @Override
   public Object read(Object object) {
-    if(group != null) {
-      group.read(object);
-    }
-    else {
-      if(reader == null) {
-        reader = Utils.getColumnReader(clazz, picklePot.getInput());
-      }
-      Utils.getUnsafe().putObject(object, offset, reader.read());
-    }
-    return object;
+    Object fieldObj = picklePot.instantiate(clazz);
+    group.read(fieldObj);
+    Utils.getUnsafe().putObject(object, offset, fieldObj);
+    return null;
+  }
+
+  @Override
+  public void flush() {
+    group.flush();
+  }
+
+  @Override
+  public void setPicklePot(NewPicklePotImpl picklePot) {
+    super.setPicklePot(picklePot);
+    group.setPicklePot(picklePot);
   }
 }
